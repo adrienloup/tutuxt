afe622acba6e305133e09d763c06402bc36553a9
Adrien Loup <adloup@gmail.com>
2021-01-30 00:23:55 +0100

    components added

---
+++ pages/component.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="tutuxt-content">
+
+    <Logo
+      v-bind="logo"
+    />
+
+    <h1>
+      TuTuxt
+    </h1>
+
+  </section>
+</template>
+
+<script lang="ts">
+import Logo from '@/components/Logo.vue';
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      logo: {
+        width: '115',
+        height: '87'
+      }
+    }
+  }
+}
+</script>
