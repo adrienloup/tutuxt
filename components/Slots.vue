752b97b6ec4a66aa08b82b7005d2469e625a9189
Adrien Loup <adloup@gmail.com>
2021-01-30 01:03:42 +0100

    components added

---
+++ components/Slots.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>
+      <slot>Slot</slot>
+    </h3>
+    <slot name="slotA">Slot A</slot>
+    <slot name="slotB">Slot B</slot>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Slot'
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: inline-block;
+  vertical-align: top;
+  margin: 0 2rem 2rem 0;
+}
+</style>
