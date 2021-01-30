1437cb283035820d04acbab677418652149d9db8
Adrien Loup <adloup@gmail.com>
2021-01-30 23:59:49 +0100

    added

---
+++ components/Popin/Popin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="popins">
+    tutu
+
+  </div>
+</template>
+
+<script>
+import Expand from '@/components/Expand/Expand';
+import ButtonPrimary from '@/components/Button/Primary';
+
+export default {
+  name: 'Popins',
+  components: { ButtonPrimary, Expand },
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
