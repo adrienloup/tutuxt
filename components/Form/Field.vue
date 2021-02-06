0655ba499b69b6f4a655e8e5926cd43ac1c703ba
Adrien Loup <adloup@gmail.com>
2021-02-07 00:07:53 +0100

    added

---
+++ components/Form/Field.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="form-field-field">
+
+    <label v-if="label" for="id">
+      {{ label }}
+    </label>
+
+    <br>
+
+    <input
+      v-bind="$attrs"
+      id="id"
+      :value="value"
+      :type="type"
+      :placeholder="placeholder"
+    />
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormFieldField',
+  props: {
+    id: {
+      type: String,
+      default: null
+    },
+    type: {
+      type: String,
+      default: null
+    },
+    value: {
+      type: String,
+      default: null
+    },
+    label: {
+      type: String,
+      default: null
+    },
+    placeholder: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
+
---
+++ components/Form/FieldText.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="field">
+
+    <div v-if="legend">
+      <slot name="legend">{{ legend }}</slot>
+    </div>
+
+    <div v-if="label">
+      <slot name="label">{{ label }}</slot>
+    </div>
+
+    <slot name="default"></slot>
+
+    <div v-if="description">
+      <slot name="description">{{ description }}</slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Field',
+  props: {
+    legend: {
+      type: String,
+      default: null
+    },
+    label: {
+      type: String,
+      default: null
+    },
+    description: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
+
