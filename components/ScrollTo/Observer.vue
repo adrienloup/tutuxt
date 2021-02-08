da219055ba533a36efcde063e8ea74bd0522844b
Adrien Loup <adloup@gmail.com>
2021-02-08 09:18:45 +0100

    added

---
+++ components/ScrollTo/Observer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    :class="{ active: active }"
+  >
+    ScrollTo
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScrollTo',
+  data() {
+    return {
+      active: false
+    };
+  }
+};
+</script>
